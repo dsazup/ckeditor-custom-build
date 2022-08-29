2211146006b0c1a24df1cc68334bc6e09732b643
Dainius Pužas <8631224+dsazup@users.noreply.github.com>
2022-08-29 19:52:51 +0300

    first commit

---
+++ src/ckeditor.js
@@ -0,0 +1,160 @@
+/**
+ * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
+import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
+import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Autoformat,
+	BlockQuote,
+	Bold,
+	Essentials,
+	Heading,
+	Highlight,
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	Indent,
+	Italic,
+	Link,
+	List,
+	MediaEmbed,
+	Paragraph,
+	PasteFromOffice,
+	Table,
+	TableToolbar,
+	TextTransformation,
+	SimpleUploadAdapter,
+	HorizontalLine,
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'link',
+			'bulletedList',
+			'numberedList',
+			'|',
+			'outdent',
+			'indent',
+			'|',
+			'imageUpload',
+			'blockQuote',
+			'insertTable',
+			'undo',
+			'redo',
+			'mediaEmbed',
+			'highlight',
+		]
+	},
+	language: 'en',
+	image: {
+		toolbar: [
+			'imageTextAlternative',
+			'imageStyle:inline',
+			'imageStyle:block',
+			'imageStyle:side'
+		]
+	},
+	table: {
+		contentToolbar: [
+			'tableColumn',
+			'tableRow',
+			'mergeTableCells'
+		]
+	},
+	highlight: {
+        options: [
+            {
+                model: 'blueMarker',
+                class: 'marker-blue',
+                title: 'Blue marker',
+                color: 'var(--ck-highlight-marker-blue)',
+                type: 'marker'
+            },
+            {
+                model: 'yellowMarker',
+                class: 'marker-yellow',
+                title: 'Yellow marker',
+                color: 'var(--ck-highlight-marker-yellow)',
+                type: 'marker'
+            },
+            {
+                model: 'redMarker',
+                class: 'marker-red',
+                title: 'Red marker',
+                color: 'var(--ck-highlight-marker-red)',
+                type: 'marker'
+            },
+            {
+                model: 'greenMarker',
+                class: 'marker-green',
+                title: 'Green marker',
+                color: 'var(--ck-highlight-marker-green)',
+                type: 'marker'
+            },
+            {
+                model: 'redPen',
+                class: 'pen-red',
+                title: 'Red pen',
+                color: 'var(--ck-highlight-pen-red)',
+                type: 'pen'
+            },
+            {
+                model: 'yellowPen',
+                class: 'pen-yellow',
+                title: 'Yellow pen',
+                color: 'var(--ck-highlight-pen-yellow)',
+                type: 'pen'
+            },           {
+                model: 'greenPen',
+                class: 'pen-green',
+                title: 'Green pen',
+                color: 'var(--ck-highlight-pen-green)',
+                type: 'pen'
+            },
+            {
+                model: 'bluePen',
+                class: 'pen-blue',
+                title: 'Blue pen',
+                color: 'var(--ck-highlight-pen-blue)',
+                type: 'pen'
+            }
+        ]
+    }
+};
+
+export default Editor;
